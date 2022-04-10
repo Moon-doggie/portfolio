3511354137691c68563d8c7418f36eb6dda5f825
Moon-doggie <82822119+Moon-doggie@users.noreply.github.com>
2022-04-11 00:40:04 +1000

    Contact Details
    Updated contact details component. Updates include changing images to icons and text updates.

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -3,30 +3,38 @@ import Phone from "../../assets/img/phone.png";
 import Email from "../../assets/img/email.png";
 import Address from "../../assets/img/address.png";
 import { contactDetails, serviceID } from "../../assets/data/data";
-import {useContext, useRef, useState} from "react"
-import emailjs from '@emailjs/browser';
+import { useContext, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { ThemeContext } from "../../context";
+import { GitHub, Home, LinkedIn, MailOutline, PhoneInTalk } from "@mui/icons-material";
 
 const Contact = () => {
-    const formRef = useRef()    
-    const [done, setDone] = useState(false)
+	const formRef = useRef();
+	const [done, setDone] = useState(false);
 
 	const theme = useContext(ThemeContext);
 	const darkMode = theme.state.darkMode;
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // send details to 
-        emailjs.sendForm(serviceID.id, serviceID.templateId, formRef.current, serviceID.userId)
-          .then((result) => {
-              console.log(result.text);
-              setDone(true)
-          }, (error) => {
-              console.log(error.text);
-          });
-    }
-
-
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// send details to
+		emailjs
+			.sendForm(
+				serviceID.id,
+				serviceID.templateId,
+				formRef.current,
+				serviceID.userId
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+					setDone(true);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+	};
 
 	return (
 		<div className="contact">
@@ -36,53 +44,65 @@ const Contact = () => {
 					<h1 className="contact-title">Let's have a chat</h1>
 					<div className="contact-info">
 						<div className="contact-info-item">
-							<img src={Phone} alt="" className="contact-icon" />
+							<PhoneInTalk className="contact-icon" />
 							{contactDetails.phone}
 						</div>
 						<div className="contact-info-item">
-							<img src={Email} alt="" className="contact-icon" />
+							<MailOutline className="contact-icon" />
 							{contactDetails.email}
 						</div>
 						<div className="contact-info-item">
-							<img
-								src={Address}
-								alt=""
-								className="contact-icon"
-							/>
+							<Home className="contact-icon" />
 							{contactDetails.address}
+						</div>
+						<div className="contact-info-item">
+							<LinkedIn className="contact-icon" />
+							<a className="link" href={contactDetails.LinkedIn}>
+								{contactDetails.linkedIn}
+							</a>
+						</div>
+						<div className="contact-info-item">
+							<GitHub className="contact-icon" />
+							<a className="link" href={contactDetails.gitHub}>
+								{contactDetails.gitHub}
+							</a>
 						</div>
 					</div>
 				</div>
 				<div className="contact-right">
 					<p className="contact-desc">
-						<b>Lorem ipsum dolor</b> sit amet consectetur
-						adipisicing elit. Laborum obcaecati nam aperiam quisquam
-						incidunt vel quo ipsam assumenda. Sunt sequi odit, neque
-						nobis impedit repellat dicta. Placeat totam quibusdam
-						sit.
+						<b>Contact me below</b> if you would like to chat
+						further about any opportunities.
 					</p>
 					<form ref={formRef} onSubmit={handleSubmit}>
-						<input style={{backgroundColor:darkMode && "#333"}} type="text" placeholder="Name" name="userName" />
-						<input style={{backgroundColor:darkMode && "#333"}}
+						<input
+							style={{ backgroundColor: darkMode && "#333" }}
+							type="text"
+							placeholder="Name"
+							name="userName"
+						/>
+						<input
+							style={{ backgroundColor: darkMode && "#333" }}
 							type="text"
 							placeholder="Subject"
 							name="userSubject"
 						/>
-						<input style={{backgroundColor:darkMode && "#333"}}
+						<input
+							style={{ backgroundColor: darkMode && "#333" }}
 							type="email"
 							placeholder="Email"
 							name="userEmail"
 						/>
 						<textarea
-                            style={{backgroundColor:darkMode && "#333"}}
+							style={{ backgroundColor: darkMode && "#333" }}
 							name="message"
 							id=""
 							cols=""
 							rows="5"
-						 	placeholder="Message"
+							placeholder="Message"
 						></textarea>
-                        <button>Submit</button>
-                        {done && "Thank you, form submitted successfully."}
+						<button>Submit</button>
+						{done && "Thank you, form submitted successfully."}
 					</form>
 				</div>
 			</div>
--- portfolio/src/components/productList/ProductList.jsx
+++ portfolio/src/components/productList/ProductList.jsx
@@ -1,21 +1,30 @@
-import Product from "../product/Product"
-import "./productList.css"
-import {products} from  "../../assets/data/data"
+import Product from "../product/Product";
+import "./productList.css";
+import { contactDetails, products } from "../../assets/data/data";
 
 const ProductList = () => {
-  return (
-    <div className="pl">
-        <div className="pl-text">
-            <h1 className="pl-title">Example Heading</h1>
-            <p className="pl-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nesciunt neque ipsam odio nihil? Perspiciatis eveniet quia ab dolor debitis minima sed ex animi, impedit, laboriosam modi obcaecati? Neque, id.</p>
-        </div>
-        <div className="pl-list">
-            {products.map((item) => (
-                <Product key={item.id} img={item.img} link={item.link}/>
-            ))}
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className="pl">
+			<div className="pl-text">
+				<h1 className="pl-title">Portfolio Projects</h1>
+				<p className="pl-desc">
+					All projects in my portfolio can be found in on my{" "}
+					<b>
+						<a href={contactDetails.GitHub} className="link">
+							GitHub
+						</a>
+					</b>
+					. To be directed to the repository for a particular project,
+					click the images below.
+				</p>
+			</div>
+			<div className="pl-list">
+				{products.map((item) => (
+					<Product key={item.id} img={item.img} link={item.link} />
+				))}
+			</div>
+		</div>
+	);
+};
 
-export default ProductList
+export default ProductList;
